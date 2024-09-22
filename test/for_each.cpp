8b355145d4bc8213670440c5e1dd11ab4432f37e
ryomashita <86085692+ryomashita@users.noreply.github.com>
2024-09-22 19:52:52 +0900

    add examples (for_each.cpp)

---
+++ test/for_each.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+
+#include <magic_enum_utility.hpp>
+
+#include "magic_enum.hpp"
+#include "sample_enums.h"
+
+TEST(MagickEnumUtilityTest, ForEach) {
+  magic_enum::enum_for_each<Color>([](auto val) {
+    constexpr Color c_color = val;
+    EXPECT_TRUE(magic_enum::enum_contains<Color>(c_color));
+  });
+}
